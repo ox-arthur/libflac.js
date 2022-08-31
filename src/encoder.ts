86a802f8f8637bef385c633a8d55cc86184708f9
Arthur Dam <arthur@mediamonks.com>
2022-08-31 16:17:39 -0700

    Lint

--- src/encoder.ts
+++ src/encoder.ts
@@ -1,7 +1,15 @@
-
-import { Flac, DestroyedEvent, CompressionLevel, encoder_write_callback_fn, metadata_callback_fn, StreamMetadata , FLAC__StreamEncoderState, CodingOptions } from '../index.d';
-import { mergeBuffers , getLength } from './utils/data-utils';
-import { BeforeReadyHandler } from './before-ready-handler';
+import {
+	Flac,
+	DestroyedEvent,
+	CompressionLevel,
+	encoder_write_callback_fn,
+	metadata_callback_fn,
+	StreamMetadata,
+	FLAC__StreamEncoderState,
+	CodingOptions,
+} from "../index.d";
+import { mergeBuffers, getLength } from "./utils/data-utils";
+import { BeforeReadyHandler } from "./before-ready-handler";
 
 export interface EncoderOptions extends EncoderResetOptions {
 	sampleRate: number;
@@ -22,16 +30,18 @@ export interface EncoderResetOptions extends CodingOptions {
 	isOgg?: boolean;
 }
 
-type ChacheableEncoderCalls = '_init' | 'encode' | 'reset';
+type ChacheableEncoderCalls = "_init" | "encode" | "reset";
 
 export class Encoder {
-
 	private _id: number | undefined;
 	private _isError: boolean = false;
 	private _isInitialized: boolean = false;
 	private _isFinished: boolean = false;
 
-	private _beforeReadyHandler?: BeforeReadyHandler<Encoder, ChacheableEncoderCalls>;
+	private _beforeReadyHandler?: BeforeReadyHandler<
+		Encoder,
+		ChacheableEncoderCalls
+	>;
 
 	/**
 	 * cache for the encoded data
@@ -66,32 +76,39 @@ export class Encoder {
 		return this._beforeReadyHandler?.isWaitOnReady || false;
 	}
 
-	constructor(private Flac: Flac, private _options: EncoderOptions){
-		this._id = Flac.create_libflac_encoder(_options.sampleRate, _options.channels, _options.bitsPerSample, _options.compression, _options.totalSamples, _options.verify);
+	constructor(private Flac: Flac, private _options: EncoderOptions) {
+		this._id = Flac.create_libflac_encoder(
+			_options.sampleRate,
+			_options.channels,
+			_options.bitsPerSample,
+			_options.compression,
+			_options.totalSamples,
+			_options.verify
+		);
 		this._onDestroyed = (evt: DestroyedEvent) => {
-			if(evt.target.id === this._id){
-				this._id = void(0);
+			if (evt.target.id === this._id) {
+				this._id = void 0;
 				this._isInitialized = false;
 				this._isFinished = false;
-				Flac.off('destroyed', this._onDestroyed);
-				if(this._beforeReadyHandler?.enabled){
+				Flac.off("destroyed", this._onDestroyed);
+				if (this._beforeReadyHandler?.enabled) {
 					this._beforeReadyHandler.enabled = false;
 				}
 			}
 		};
-		Flac.on('destroyed', this._onDestroyed);
+		Flac.on("destroyed", this._onDestroyed);
 
 		this._onWrite = (data: Uint8Array) => {
 			this.addData(data);
 		};
 
 		this._onMetaData = (m?: StreamMetadata) => {
-			if(m){
+			if (m) {
 				this._metadata = m;
 			}
 		};
 
-		if(this._id === 0){
+		if (this._id === 0) {
 			this._isError = true;
 		} else {
 			// if(this._isAnalyse(this._options)){
@@ -101,19 +118,27 @@ export class Encoder {
 		}
 	}
 
-	private _init(isEncodeOgg?: boolean) : void {
-		if(this._id){
-			const state = isEncodeOgg?
-				this.Flac.init_encoder_ogg_stream(this._id, this._onWrite, this._onMetaData):
-				this.Flac.init_encoder_stream(this._id, this._onWrite, this._onMetaData);
+	private _init(isEncodeOgg?: boolean): void {
+		if (this._id) {
+			const state = isEncodeOgg
+				? this.Flac.init_encoder_ogg_stream(
+						this._id,
+						this._onWrite,
+						this._onMetaData
+				  )
+				: this.Flac.init_encoder_stream(
+						this._id,
+						this._onWrite,
+						this._onMetaData
+				  );
 
 			this._isError = state !== 0;
-			if(state === 0){
+			if (state === 0) {
 				this._isInitialized = true;
 				this._isFinished = false;
 			}
 		} else {
-			this._handleBeforeReady('_init', arguments);
+			this._handleBeforeReady("_init", arguments);
 		}
 	}
 
@@ -122,26 +147,31 @@ export class Encoder {
 	 * resets internal state and clears cached input/output data.
 	 */
 	public reset(options?: EncoderResetOptions): boolean {
-		if(this._id){
-
+		if (this._id) {
 			// do reset encoder, if it was initialized -> call finished()
-			let state: FLAC__StreamEncoderState = this.Flac.FLAC__stream_encoder_get_state(this._id);
-			if(state === /* FLAC__STREAM_ENCODER_OK */ 0){
+			let state: FLAC__StreamEncoderState =
+				this.Flac.FLAC__stream_encoder_get_state(this._id);
+			if (state === /* FLAC__STREAM_ENCODER_OK */ 0) {
 				this.Flac.FLAC__stream_encoder_finish(this._id);
 				state = this.Flac.FLAC__stream_encoder_get_state(this._id);
 			}
 
-			if(state === /* FLAC__STREAM_ENCODER_UNINITIALIZED */ 1){
-
-				if(options){
-
+			if (state === /* FLAC__STREAM_ENCODER_UNINITIALIZED */ 1) {
+				if (options) {
 					Object.assign(this._options, options);
 					// if(this._isAnalyse(this._options)){
 					// 	Flac.setOptions(this._id, this._options);
 					// }
 
-					if(typeof options.verify !== 'undefined' && this.Flac.FLAC__stream_encoder_get_verify(this._id) != /*non-exact comparision*/ this._options.verify){
-						this.Flac.FLAC__stream_encoder_set_verify(this._id, !!this._options.verify);
+					if (
+						typeof options.verify !== "undefined" &&
+						this.Flac.FLAC__stream_encoder_get_verify(this._id) !=
+							/*non-exact comparision*/ this._options.verify
+					) {
+						this.Flac.FLAC__stream_encoder_set_verify(
+							this._id,
+							!!this._options.verify
+						);
 					}
 
 					// TODO unsupported as of yet:
@@ -157,8 +187,11 @@ export class Encoder {
 					// if(typeof options.totalSamples !== 'number'){
 					// 	this.Flac.FLAC__stream_encoder_set_total_samples_estimate(this._id, this._options.totalSamples);
 					// }
-					if(typeof options.compression !== 'number'){
-						this.Flac.FLAC__stream_encoder_set_compression_level(this._id, this._options.compression);
+					if (typeof options.compression !== "number") {
+						this.Flac.FLAC__stream_encoder_set_compression_level(
+							this._id,
+							this._options.compression
+						);
 					}
 				}
 				this.clearData();
@@ -170,7 +203,7 @@ export class Encoder {
 				return this._isError;
 			}
 		}
-		return this._handleBeforeReady('reset', arguments);
+		return this._handleBeforeReady("reset", arguments);
 	}
 
 	/** finish encoding: the encoder needs to be reset or destroyed afterwards. */
@@ -193,7 +226,11 @@ export class Encoder {
 	 *
 	 * @throws Error in case non-interleaved encoding data did not match the number of expected channels
 	 */
-	public encode(pcmData: Int32Array[], numberOfSamples: number | undefined, isInterleaved: false): boolean;
+	public encode(
+		pcmData: Int32Array[],
+		numberOfSamples: number | undefined,
+		isInterleaved: false
+	): boolean;
 	/**
 	 * encode interleaved PCM data to FLAC
 	 * @param  pcmData the PCM data (interleaved: one sample for each channel)
@@ -212,7 +249,11 @@ export class Encoder {
 	 *
 	 * @throws Error in case non-interleaved encoding data did not match the number of expected channels
 	 */
-	public encode(pcmData: Int32Array, numberOfSamples: number | undefined, isInterleaved: true): boolean;
+	public encode(
+		pcmData: Int32Array,
+		numberOfSamples: number | undefined,
+		isInterleaved: true
+	): boolean;
 	/**
 	 * encode PCM data to FLAC
 	 * @param  pcmData the PCM data: either interleaved, or an array of the channels
@@ -222,42 +263,62 @@ export class Encoder {
 	 *
 	 * @throws Error in case non-interleaved encoding data did not match the number of expected channels
 	 */
-	public encode(pcmData?: Int32Array | Int32Array[], numberOfSamples?: number, isInterleaved?: boolean): boolean {
-		if(this._id && this._isInitialized && !this._isFinished){
-
+	public encode(
+		pcmData?: Int32Array | Int32Array[],
+		numberOfSamples?: number,
+		isInterleaved?: boolean
+	): boolean {
+		if (this._id && this._isInitialized && !this._isFinished) {
 			// console.log('encoding with ', this._options, pcmData);
 
-			if(typeof pcmData === 'undefined'){
+			if (typeof pcmData === "undefined") {
 				// console.log('finish encoding...');
 				return this._finish();
 			}
 
-			if(typeof isInterleaved === 'undefined'){
+			if (typeof isInterleaved === "undefined") {
 				// console.log('determining interleaved ...');
-				isInterleaved = !(Array.isArray(pcmData) && pcmData[0] instanceof Int32Array);
+				isInterleaved = !(
+					Array.isArray(pcmData) && pcmData[0] instanceof Int32Array
+				);
 				// console.log('is interleaved?: ', isInterleaved);
 			}
 
-			if(typeof numberOfSamples === 'undefined'){
+			if (typeof numberOfSamples === "undefined") {
 				// console.log('calculating numberOfSamples...');
 				// const byteNum = this._options.bitsPerSample / 8;
-				const buff = isInterleaved? (pcmData as Int32Array) : (pcmData as Int32Array[])[0];
+				const buff = isInterleaved
+					? (pcmData as Int32Array)
+					: (pcmData as Int32Array[])[0];
 				// console.log('calculating numberOfSamples: byteNum='+byteNum+' for buffer ', buff);
-				numberOfSamples = (buff.byteLength - buff.byteOffset) / ((isInterleaved? this._options.channels : 1) * buff.BYTES_PER_ELEMENT);// * byteNum);
+				numberOfSamples =
+					(buff.byteLength - buff.byteOffset) /
+					((isInterleaved ? this._options.channels : 1) *
+						buff.BYTES_PER_ELEMENT); // * byteNum);
 			}
 
-			if(isInterleaved){
+			if (isInterleaved) {
 				// console.log('encoding interleaved ('+numberOfSamples+' samples)...');
-				return !!this.Flac.FLAC__stream_encoder_process_interleaved(this._id, pcmData as Int32Array, numberOfSamples);
+				return !!this.Flac.FLAC__stream_encoder_process_interleaved(
+					this._id,
+					pcmData as Int32Array,
+					numberOfSamples
+				);
 			}
 			// ASSERT encode non-interleaved
-			if(this._options.channels !== pcmData.length){
-				throw new Error(`Wrong number of channels: expected ${this._options.channels} but got ${pcmData.length}`)
+			if (this._options.channels !== pcmData.length) {
+				throw new Error(
+					`Wrong number of channels: expected ${this._options.channels} but got ${pcmData.length}`
+				);
 			}
 			// console.log('encoding non-interleaved ('+numberOfSamples+' samples)...');
-			return !!this.Flac.FLAC__stream_encoder_process(this._id, pcmData as Int32Array[], numberOfSamples);
+			return !!this.Flac.FLAC__stream_encoder_process(
+				this._id,
+				pcmData as Int32Array[],
+				numberOfSamples
+			);
 		}
-		return this._handleBeforeReady('encode', arguments);
+		return this._handleBeforeReady("encode", arguments);
 	}
 
 	public getSamples(): Uint8Array {
@@ -265,18 +326,18 @@ export class Encoder {
 	}
 
 	public getState(): FLAC__StreamEncoderState | -1 {
-		if(this._id){
+		if (this._id) {
 			return this.Flac.FLAC__stream_encoder_get_state(this._id);
 		}
 		return -1;
 	}
 
 	public destroy(): void {
-		if(this._id){
+		if (this._id) {
 			this.Flac.FLAC__stream_encoder_delete(this._id);
 		}
 		this._beforeReadyHandler && (this._beforeReadyHandler.enabled = false);
-		this._metadata = void(0);
+		this._metadata = void 0;
 		this.clearData();
 	}
 
@@ -289,20 +350,22 @@ export class Encoder {
 	}
 
 	private _finish(): boolean {
-		if(this._id && this._isInitialized && !this._isFinished){
-			if(!!this.Flac.FLAC__stream_encoder_finish(this._id)){
+		if (this._id && this._isInitialized && !this._isFinished) {
+			if (!!this.Flac.FLAC__stream_encoder_finish(this._id)) {
 				this._isFinished = true;
 				return true;
-			};
+			}
 		}
 		return false;
 	}
 
-	private _handleBeforeReady(funcName: ChacheableEncoderCalls, args: ArrayLike<any>): boolean {
-		if(this._beforeReadyHandler){
+	private _handleBeforeReady(
+		funcName: ChacheableEncoderCalls,
+		args: ArrayLike<any>
+	): boolean {
+		if (this._beforeReadyHandler) {
 			return this._beforeReadyHandler.handleBeforeReady(funcName, args);
 		}
 		return false;
 	}
-
 }
